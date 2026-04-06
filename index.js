4fa95396abd363025f09f2cfd3769309974d559f
geodome21 <gibson.h.matt@gmail.com>
2026-04-06 20:37:33 +0000

    FAHHHHH

--- index.js
+++ index.js
@@ -95,6 +95,7 @@ const games=[
 {title:"Gladihoppers",img:"images/Gladihoppers.png",url:"games/Gladihoppers/index.html",cat:"all"},
 {title:"Gold Digger FRVR",img:"images/Gold Digger FRVR.png",url:"games/Gold Digger FRVR/index.html",cat:"all"},
 {title:"Google Dino",img:"images/Google Dino.png",url:"games/Google Dino/index.html",cat:"all"},
+{title:"Granny",img:"images/Granny.png",url:"games/Granny/index.html",cat:"all"},
 {title:"High Stakes",img:"images/High Stakes.png",url:"games/High Stakes/index.html",cat:"all"},
 {title:"Icy Dodo",img:"images/Icy Dodo.png",url:"games/Icy Dodo/index.html",cat:"all"},
 {title:"Iron Lung",img:"images/Iron Lung.png",url:"games/Iron Lung/index.html",cat:"all"},
