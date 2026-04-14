687640ed3aa92f9002977b59f5f51dd87334bbb9
geodome21 <gibson.h.matt@gmail.com>
2026-04-14 21:31:09 +0000

    Slice Master RIP

--- index.js
+++ index.js
@@ -132,7 +132,6 @@ const games=[
 {title:"Retro Bowl",img:"images/Retro Bowl.png",url:"games/Retro Bowl/index.html",cat:"all"},
 {title:"Riddle School",img:"images/Riddle School.png",url:"games/Riddle School/index.html",cat:"all"},
 {title:"Silk",img:"images/Silk.png",url:"games/Silk/index.html",cat:"all"},
-{title:"Slice Master",img:"images/Slice Master.png",url:"games/Slice Master/index.html",cat:"all"},
 {title:"Slope Plus",img:"images/Slope Plus.png",url:"games/Slope Plus/index.html",cat:"all"},
 {title:"Smash Karts",img:"images/Smash Karts.jpg",url:"games/Smash Karts/index.html",cat:"all"},
 {title:"Spacebar Clicker",img:"images/Spacebar Clicker.png",url:"games/Spacebar Clicker/index.html",cat:"all"},
