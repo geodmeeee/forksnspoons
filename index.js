466de176c010e8a7c151451354284aed1fc09fe8
geodmeee <gibson.h.matt@gmail.com>
2026-05-11 20:47:47 +0000

    Crazy CARS i be meaning to add dis for ages ong

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ const games=[
 {title:"Candy Crush",img:"images/Candy Crush.png",url:"games/Candy Crush/index.html",cat:"all"},
 {title:"Cluster Rush",img:"images/Cluster Rush.png",url:"games/Cluster Rush/index.html",cat:"all"},
 {title:"Core Ball",img:"images/Core Ball.png",url:"games/Core Ball/index.html",cat:"all"},
+{title:"Crazy Cars",img:"images/Crazy Cars.png",url:"games/Crazy Cars/index.html",cat:"all"},
 {title:"Crazy Cattle 3D",img:"images/Crazy Cattle 3D.png",url:"games/Crazy Cattle 3D/index.html",cat:"all"},
 {title:"CS GO Clicker",img:"images/CS GO clicker.png",url:"games/CS GO clicker/index.html",cat:"all"},
 {title:"Cyber Cars Punk Racing",img:"images/Cyber Cars Punk Racing.png",url:"games/Cyber Cars Punk Racing/index.html",cat:"all"},
